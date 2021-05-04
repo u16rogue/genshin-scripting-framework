7a0050be75a8762ce4c06a8f9e5ae163ae24616a
rogueeeee <61908580+rogueeeee@users.noreply.github.com>
2021-05-04 20:33:00 +0800

    implemented utils::aob_scan

--- utils/aob_scan.cpp
+++ utils/aob_scan.cpp
@@ -3,9 +3,27 @@
 
 std::uint8_t *utils::aob_scan(void *start, std::size_t size, const char *signature, const char *mask)
 {
-	std::size_t byte_count = 0;
-	if ( !start || size < 1 || !signature || !mask || std::strlen(signature) != (byte_count = std::strlen(mask)) )
+	if ( !start || size < 1 || !signature || !mask )
 		return nullptr;
 
+	std::uint8_t      *current_address = reinterpret_cast<std::uint8_t *>(start);
+	const std::size_t  byte_count      = std::strlen(mask);
+	const std::uint8_t *end            = current_address + size;
 
+	do
+	{
+		for (int i = 0; i <= byte_count; i++)
+		{
+			if (mask[i] == '?')
+				continue;
+
+			if (mask[i] != 'x' || current_address[i] != reinterpret_cast<const unsigned char *>(signature)[i])
+				break;
+
+			if (mask[i + 1] == '\0')
+				return current_address;
+		}
+	} while (++current_address + byte_count <= end);
+
+	return nullptr;
 }
