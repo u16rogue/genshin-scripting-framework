f6ad1ed666991551fac0b890d991eed12f39865f
rogueeeee <61908580+rogueeeee@users.noreply.github.com>
2021-05-05 15:34:52 +0800

    added minhook, disabled utils/hook, disabled utils::hook_nearcall64

--- utils/hook/hook_nearcall64.cpp
+++ utils/hook/hook_nearcall64.cpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#if 0
+
 #include <Windows.h>
 #include <cstdint>
 #include <cstddef>
-#include "../hook.h"
-#include <bit>
+#include "hook_nearcall64.h"
 
 bool utils::hook_nearcall64(void *instruction_address, void *hook_address, void **original_address_out)
 {
@@ -17,32 +18,36 @@ bool utils::hook_nearcall64(void *instruction_address, void *hook_address, void 
 	if ( !instruction_address || !hook_address )
 		return false;
 
+	// Since we're using an x86 near call jump instruction, check if the offset is within range for a 4 byte offset
+	if ( reinterpret_cast<std::uintptr_t>(hook_address) - next_op > 0x00000000FFFFFFFF )
+		return false;
+
 	// Allow read, write, and execute
 	DWORD o_prot = NULL;
 	if ( !VirtualProtect(instruction_address, instruction_size, PAGE_EXECUTE_READWRITE, &o_prot) )
 		return false;
-
-	// Check x64 near call opcode (FF 15)
-	if ( *opcode != 0x15FF )
+	
+	if ( *opcode == 0x15FF )
+		*opcode = 0xE890;// Replace the x64 near call to an x86 near call
+	else if ( *opcode == 0xE890 )
+		*opcode = 0x15FF; // Should allow us to "rehook" the call instruction with the original address which basically is unhooking it
+	else
 		return false;
 
 	// Return the original address. Calculated by adding the address of the next instruction to the call operand.
 	if ( original_address_out )
 	{
-		*original_address_out = reinterpret_cast<void *>(next_op + *operand);
+		*original_address_out = reinterpret_cast<void*>(next_op + *operand);
 
-		// Check if the destination address contains a SUB RSP (48 83 EC) instruction, if it doesn't dereference the current original address.
+		// Check if the destination address contains a SUB RSP (48 83 EC) instruction, if it doesn't dereference the current original address unless it goes to a jump table.
 		// NOTE: does not apply for every function
 		// TODO: implement this properly for other functions
-		if ((*reinterpret_cast<std::uint32_t*>(hook_address) & 0x00FFFFFF) != 0x00EC8348)
+		if ( (*reinterpret_cast<std::uint32_t*>(hook_address) & 0x00FFFFFF) != 0x00EC8348 && *reinterpret_cast<std::uint8_t*>(hook_address) != 0xE9 )
 			*original_address_out = *reinterpret_cast<void**>(*original_address_out);
 	}
 
 	// Hook the call by placing the offset of the hook address
 	*operand = (reinterpret_cast<std::uintptr_t>(hook_address) - next_op) & 0xFFFFFFFF;
-
-	// Replace the x64 near call to an x86 near call
-	 *opcode = 0xE890;
 
 	// Restore the original protection
 	if ( !VirtualProtect(instruction_address, instruction_size, o_prot, &o_prot) )
@@ -50,3 +55,5 @@ bool utils::hook_nearcall64(void *instruction_address, void *hook_address, void 
 
 	return true;
 }
+
+#endif
--- utils/hook/hook_nearcall64.h
+++ utils/hook/hook_nearcall64.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#if 0
+
 namespace utils
 {
 	bool hook_nearcall64(void *instruction_address, void *hook_address, void **original_address_out);
 }
+
+#endif
