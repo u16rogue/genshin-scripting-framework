c055e223bd14e462f7fb1c0023ff3b4af1e400da
rogueeeee <61908580+rogueeeee@users.noreply.github.com>
2021-05-10 18:08:49 +0800

    added utils::get_full_path, utils::remote_execute, utils::remote_allocate

--- utils/winapi_helper.cpp
+++ utils/winapi_helper.cpp
@@ -20,3 +20,94 @@ bool utils::open_winthread::resume()
 {
 	return ResumeThread(this->thread_handle) != 0xFFFFFFFF;
 }
+
+utils::remote_allocate::remote_allocate(HANDLE proc_handle_, std::size_t alloc_size_, DWORD page_prot_)
+	: proc_handle(proc_handle_), alloc_size(alloc_size_), page_prot(page_prot_)
+{
+	this->alloc();
+}
+
+utils::remote_allocate::~remote_allocate()
+{
+	if (this->ptr)
+		this->free();
+}
+
+bool utils::remote_allocate::alloc(HANDLE proc_handle_, std::size_t alloc_size_, DWORD page_prot_)
+{
+	if (proc_handle_)
+		this->proc_handle = proc_handle_;
+
+	if (alloc_size_)
+		this->alloc_size = alloc_size_;
+
+	if (page_prot_)
+		this->page_prot = page_prot_;
+
+	this->ptr = VirtualAllocEx(proc_handle, nullptr, alloc_size, MEM_COMMIT | MEM_RESERVE, page_prot);
+	return this->ptr != nullptr;
+}
+
+bool utils::remote_allocate::free()
+{
+	if (VirtualFreeEx(proc_handle, this->ptr, NULL, MEM_RELEASE))
+	{
+		this->ptr = nullptr;
+		return true;
+	}
+	
+	return false;
+}
+
+std::wstring utils::get_full_path(std::wstring_view initial_path)
+{
+	wchar_t path_buffer[MAX_PATH] = { '\0' };
+
+	if (!GetFullPathNameW(initial_path.data(), sizeof(path_buffer) / sizeof(wchar_t), path_buffer, nullptr))
+		return std::wstring();
+
+	return std::wstring(path_buffer);
+}
+
+utils::remote_execute::remote_execute(HANDLE proc_handle_, LPVOID routine_adr_, void *arg_ptr_)
+	: proc_handle(proc_handle_), routine_adr(routine_adr_), arg_ptr(arg_ptr_)
+{
+}
+
+utils::remote_execute::~remote_execute()
+{
+	if (this->crt_handle)
+	{
+		CloseHandle(this->crt_handle);
+		this->crt_handle = nullptr;
+	}
+}
+
+bool utils::remote_execute::execute(HANDLE proc_handle_, LPVOID routine_adr_, void *arg_ptr_)
+{
+	if (proc_handle_)
+		this->proc_handle = proc_handle_;
+
+	if (routine_adr_)
+		this->routine_adr = routine_adr_;
+
+	if (arg_ptr_)
+		this->arg_ptr = arg_ptr_;
+
+	this->crt_handle = CreateRemoteThread(this->proc_handle, nullptr, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(this->routine_adr), this->arg_ptr, NULL, nullptr);
+	return this->crt_handle != nullptr;
+}
+
+void utils::remote_execute::wait(DWORD timeout)
+{
+	if (this->crt_handle)
+		WaitForSingleObject(this->crt_handle, timeout);
+}
+
+bool utils::remote_execute::execute_wait(DWORD timeout, HANDLE proc_handle_, LPVOID routine_adr_, void *arg_ptr_)
+{
+	this->execute(proc_handle_, routine_adr_, arg_ptr_);
+	this->wait(timeout);
+
+	return this->crt_handle != nullptr;
+}
--- utils/winapi_helper.h
+++ utils/winapi_helper.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <Windows.h>
+#include <cstdint>
+#include <cstddef>
+#include <string>
 
 namespace utils
 {
@@ -12,7 +15,7 @@ namespace utils
 
 		operator bool() const
 		{
-			return !!thread_handle;
+			return this->thread_handle != nullptr;
 		}
 
 		bool suspend();
@@ -21,4 +24,47 @@ namespace utils
 	private:
 		HANDLE thread_handle = nullptr;
 	};
+
+	class remote_allocate
+	{
+	public:
+		remote_allocate() = default;
+		remote_allocate(HANDLE proc_handle_, std::size_t alloc_size_, DWORD page_prot_ = PAGE_READWRITE);
+		~remote_allocate();
+
+		operator bool() const
+		{
+			return this->ptr != nullptr;
+		}
+
+		bool alloc(HANDLE proc_handle_ = nullptr, std::size_t alloc_size_ = 0, DWORD page_prot_ = NULL);
+		bool free();
+
+		LPVOID ptr = nullptr;
+
+	private:
+		HANDLE      proc_handle = nullptr;
+		std::size_t alloc_size  = 0;
+		DWORD       page_prot   = PAGE_READWRITE;
+	};
+
+	class remote_execute
+	{
+	public:
+		remote_execute() = default;
+		remote_execute(HANDLE proc_handle_, LPVOID routine_adr_, void *arg_ptr_ = nullptr);
+		~remote_execute();
+
+		bool execute(HANDLE proc_handle_ = nullptr, LPVOID routine_adr_ = nullptr, void *arg_ptr_ = nullptr);
+		void wait(DWORD timeout = INFINITE);
+		bool execute_wait(DWORD timeout = INFINITE, HANDLE proc_handle_ = nullptr, LPVOID routine_adr_ = nullptr, void *arg_ptr_ = nullptr);
+
+	private:
+		HANDLE  proc_handle = nullptr;
+		LPVOID  routine_adr = nullptr;
+		void   *arg_ptr     = nullptr;
+		HANDLE  crt_handle  = nullptr;
+	};
+
+	std::wstring get_full_path(std::wstring_view initial_path);
 }
