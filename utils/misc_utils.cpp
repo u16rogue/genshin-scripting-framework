11b691c34a0c6ffb8a33024d9da8ebf27d5ab86d
rogueeeee <61908580+rogueeeee@users.noreply.github.com>
2021-06-12 22:21:24 +0800

    auto invoke bypass, removed test hooks, added utils::random_str

--- utils/misc_utils.cpp
+++ utils/misc_utils.cpp
@@ -1,8 +1,22 @@
 #include "misc_utils.h"
 #include <Windows.h>
+#include <ctime>
 
 void utils::spinlock_key(int key, unsigned int sleepms)
 {
 	while (!GetAsyncKeyState(key))
 		Sleep(sleepms);
+}
+
+std::wstring utils::random_str(int length, std::wstring_view char_set)
+{
+	std::srand(std::time(nullptr));
+	const std::size_t set_max_idx = char_set.length();
+	std::wstring result;
+	result.reserve(length + 1);
+
+	for (int i = 0; i < length; i++)
+		result += char_set[std::rand() % set_max_idx];
+
+	return result;
 }
--- utils/misc_utils.h
+++ utils/misc_utils.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <string>
+
 namespace utils
 {
 	void spinlock_key(int key = 0x20 /*VK_SPACE*/, unsigned int sleepms = 100);
+
+	std::wstring random_str(int length = 12, std::wstring_view char_set = L"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
 }
