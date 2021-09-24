5c7183372db1b22e00aadad437b106b5c761eff8
u16rogue <u16rogue@waifu.club>
2021-09-24 12:14:04 +0800

    added imgui.inputtext and imgui.checkbox

--- gsf-client/api/api_imgui.cpp
+++ gsf-client/api/api_imgui.cpp
@@ -4,8 +4,12 @@
 bool gsf::api_imgui::setup_api(sol::state &slua)
 {
 	auto namespace_imgui = slua["imgui"].get_or_create<sol::table>();
+
 	namespace_imgui.set_function("begin", &gsf::api_imgui::_api_begin, this);
 	namespace_imgui.set_function("iend", &gsf::api_imgui::_api_iend, this);
+	namespace_imgui.set_function("checkbox", &gsf::api_imgui::_api_checkbox, this);
+	namespace_imgui.set_function("inputtext", &gsf::api_imgui::_api_inputtext, this);
+
 	namespace_imgui.set_function("text", [](const char *text) { ImGui::Text(text); });
 	namespace_imgui.set_function("same_line", []() { ImGui::SameLine(); });
 	namespace_imgui.set_function("button", [](const char *text) -> bool { return ImGui::Button(text); });
@@ -26,4 +30,18 @@ void gsf::api_imgui::_api_iend()
 {
 	--this->imgui_active_begin_count;
 	return ImGui::End();
+}
+
+bool gsf::api_imgui::_api_checkbox(const char *label)
+{
+	auto &container = *reinterpret_cast<bool *>(&this->imgui_data_ref[label]);
+	ImGui::Checkbox(label, &container);
+	return container;
+}
+
+const char *gsf::api_imgui::_api_inputtext(const char *label)
+{
+	auto &container = this->imgui_data_ref[label];
+	ImGui::InputText(label, container, sizeof(container));
+	return container;
 }
