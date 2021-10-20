5581849ab6ffa3d56b9005bf381d3eed5f75faca
u16rogue <u16rogue@waifu.club>
2021-10-20 22:50:59 +0800

    added imgui.set_data_ref_string, imgui.set_data_ref_bool, imgui.set_data_ref_float, imgui.slider_float

--- gsf-client/api/api_imgui.h
+++ gsf-client/api/api_imgui.h
@@ -15,12 +15,27 @@ namespace gsf
 		inline static std::int64_t imgui_active_begin_count = 0;
 
 	private:
-		std::unordered_map<const char *, char[128]> imgui_data_ref;
+		using data_ref_t = char[128];
+		std::unordered_map<const char *, data_ref_t> imgui_data_ref;
 
 	private:
+
+		template <typename T>
+		void _api_set_data_ref(const char *label, T value)
+		{
+			*reinterpret_cast<T*>(&this->imgui_data_ref[label]) = value;
+		};
+
+		template <>
+		void _api_set_data_ref(const char *label, const char *value)
+		{
+			strncpy_s(this->imgui_data_ref[label], value, sizeof(data_ref_t));
+		};
+
 		bool _api_begin(const char *text);
 		void _api_iend();
 		bool _api_check_box(const char *label);
 		const char *_api_input_text(const char *label);
+		float _api_slider_float(const char *label, float min, float max);
 	};
 }
